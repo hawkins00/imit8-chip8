1338ecd731ca3fef29a4a21cc79d918eeb922fb4
Hawkins <haakenson@gmail.com>
2018-07-07 13:03:44 -0700

    Refactor magic numbers, started main loop & chip8.init

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "chip8.h"
 
 int main(int argc, char* argv[])
 {
+    // TODO: write log statements for stuff that happens
+    chip8 cpu0;
     std::cout << "Hello, World!" << std::endl;
+
+    while(true)
+    {
+        break;
+    }
     return 0;
 }
