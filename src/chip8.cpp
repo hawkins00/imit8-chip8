5bbddc61dbdef203ae5110f541f83de3731eeeb7
Hawkins <haakenson@gmail.com>
2018-07-21 16:40:14 -0700

    Added a bunch of opcodes

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -3,6 +3,8 @@
  * Implementation of the Chip-8 CPU Core.
  */
 
+// XXX: seriously rethink this uint_fastX_t junk :/
+
 #include "chip8.h"
 
 chip8::
@@ -131,48 +133,182 @@ bool chip8::
 decode()
 {
     // TODO: Change ifs to case or something even grander?
-    // 1XXX (goto)
-    if ((opCode >> 12) == 0x1)
+    switch (opCode >> 12)
     {
-        progCounter = opCode & 0x0FFF;
-        // check segfault (is this necessary?) or odd address
-        if (progCounter < CODE_START || progCounter % 2)
-        {
-            return false;
-        }
-        std::cout << "GOTO: " << std::hex << progCounter << std::endl;
-    }
-    // 2XXX (subroutine call)
-    else if ((opCode >> 12) == 0x2)
-    {
-        callStack.push(progCounter);
-        progCounter = opCode & 0x0FFF;
+        case 0x0:
+            // 0XXX ()
+            std::cout << "0x0" << std::endl;
+            break;
 
-        // stack overflow or segfault or odd address
-        if (callStack.size() > STACK_DEPTH || progCounter < CODE_START || progCounter % 2)
-        {
-            return false;
-        }
+        // 0x1XXX (goto)
+        case 0x1:
+            progCounter = opCode & 0x0FFF;
+            // check segfault (is this necessary?) or odd address
+            if (progCounter < CODE_START || progCounter % 2)
+            {
+                return false;
+            }
+            std::cout << "GOTO: " << std::hex << progCounter << std::endl;
+        break;
 
-        std::cout << "CALL: " << std::hex << progCounter << std::endl;
-    }
-    // FX15 (soundInterruptTimer = registers[X])
-    else if ((opCode & 0xF0FF) == 0xF015)
-    {
-        soundInterruptTimer = static_cast<uint_fast8_t>(registers[(opCode >> 8) & 0xF]);
-        progCounter += 2;
-        //std::cout << std::hex << ((opCode >> 8) & 0x0F) << std::endl;
-        //std::cout << "Sound: " << std::hex << (int)soundInterruptTimer << std::endl;
-    }
-    else
-    {
-        progCounter += 2;
+        // 0x2XXX (subroutine call)
+        case 0x2:
+            callStack.push(progCounter);
+            progCounter = opCode & 0x0FFF;
+
+            // stack overflow or segfault or odd address
+            if (callStack.size() > STACK_DEPTH || progCounter < CODE_START || progCounter % 2)
+            {
+                return false;
+            }
+
+            std::cout << "CALL: " << std::hex << progCounter << std::endl;
+            break;
+
+        // 0x3RXX (skip next opcode if registers[R] == XX
+        case 0x3:
+            if (registers[(opCode >> 8) & 0xF] == opCode & 0xFF)
+            {
+                progCounter += 4;
+                std::cout << "0x3 (skip next)" << std::endl;
+            }
+            else
+            {
+                progCounter += 2;
+                std::cout << "0x3 (don't skip next)" << std::endl;
+            }
+            break;
+
+        // 0x4RXX (skip next opcode if registers[R] != XX
+        case 0x4:
+            if (registers[(opCode >> 8) & 0xF] != opCode & 0xFF)
+            {
+                progCounter += 4;
+                std::cout << "0x4RXX (skip next)" << std::endl;
+            }
+            else
+            {
+                progCounter += 2;
+                std::cout << "0x4RXX (don't skip next)" << std::endl;
+            }
+            break;
+
+        // 0x5RS0 (skip next opcode if registers[R] == registers[S]
+        case 0x5:
+            switch (opCode & 0xF)
+            {
+                case (0):
+                    if (registers[(opCode >> 8) & 0xF] == registers[(opCode >> 4)] & 0xF)
+                    {
+                        progCounter += 4;
+                        std::cout << "0x5RS0 (skip next)" << std::endl;
+                    }
+                    else
+                    {
+                        progCounter += 2;
+                        std::cout << "0x5RS0 (don't skip next)" << std::endl;
+                    }
+                    break;
+                default:
+                    // TODO: error
+                    return false;
+            }
+            break;
+
+        // 0x6RXX (set registers[R] = XX)
+        case 0x6:
+            registers[(opCode >> 8) & 0xF] = static_cast<uint_fast8_t>(opCode & 0xFF);
+            progCounter += 2;
+            std::cout << "0x6RXX" << std::endl;
+            break;
+
+        // 0xFRXX
+        case 0xF:
+            switch (opCode & 0xFF)
+            {
+                // 0xFR07 (registers[R] = delayInterruptTimer)
+                case 0x07:
+                    registers[(opCode >> 8) & 0xF] = delayInterruptTimer;
+                    progCounter += 2;
+                    std::cout << "0xFR07" << std::endl;
+                    break;
+
+                // FIXME (maybe?): Who knows if this is a correct way to get the keypress
+                // 0xFR0A (execution waits for keypress, stored in registers[R])
+                case 0x0A:
+                    std::cout << "0xFR0A: waiting for keypress..." << std::endl;
+                    uint_fast8_t tempChar;
+                    do
+                    {
+                        tempChar = static_cast<uint_fast8_t>(getchar() & 0xFF);
+                    } while (!isxdigit(tempChar));
+                    registers[(opCode >> 8) & 0xF] = tempChar;
+                    progCounter += 2;
+                    std::cout << "0xFR0A" << std::endl;
+                    break;
+
+                // 0xFR15 (delayInterruptTimer = registers[R])
+                case 0x15:
+                    delayInterruptTimer = static_cast<uint_fast8_t>(registers[(opCode >> 8) & 0xF]);
+                    progCounter += 2;
+                    //std::cout << std::hex << ((opCode >> 8) & 0x0F) << std::endl;
+                    //std::cout << "Delay: " << std::hex << (int)delayInterruptTimer << std::endl;
+                    break;
+
+                // 0xFR18 (soundInterruptTimer = registers[R])
+                case 0x18:
+                    soundInterruptTimer = static_cast<uint_fast8_t>(registers[(opCode >> 8) & 0xF]);
+                    progCounter += 2;
+                    //std::cout << std::hex << ((opCode >> 8) & 0x0F) << std::endl;
+                    //std::cout << "Sound: " << std::hex << (int)soundInterruptTimer << std::endl;
+                    break;
+
+                // 0xFR1E (index += registers[R])
+                case 0x1E:
+                    index += registers[(opCode >> 8) & 0xF];
+                    progCounter += 2;
+                    std::cout << "0xFR1E: Index += Reg[]" << std::endl;
+                    break;
+
+                // 0xFR29 (set index to address of sprite for character in registers[R])
+                case 0x29:
+                    // TODO (?): check if index is legal
+                    index = static_cast<uint_fast16_t>(registers[(opCode >> 8) & 0xF] * 5);
+                    progCounter += 2;
+                    std::cout << "0xFR29: index = font sprite address" << std::endl;
+                    break;
+
+                // 0xFR33 (set index to address of sprite for character in registers[R])
+                case 0x33:
+                { // scope limiter ({), so tempNum doesn't cross initialization
+                    uint_fast8_t tempNum = registers[(opCode >> 8) & 0xF];
+                    memory[index] = static_cast<uint_fast8_t>(tempNum / 100);
+                    memory[index + 1] = static_cast<uint_fast8_t>(tempNum / 10 % 10);
+                    memory[index + 2] = static_cast<uint_fast8_t>(tempNum % 10);
+                    progCounter += 2;
+                    std::cout << "0xFR33" << std::endl;
+                    break;
+                }
+
+                // opCode is not implemented, so crash already
+                default:
+                    std::cout << "OpCode not implemented" << std::endl;
+                    return false;
+            }
+            break;
+
+        default:
+        {
+            progCounter += 2;
+            std::cout << "default" << std::endl;
+        }
     }
 
     std::cout << "PC: " << std::hex << progCounter << std::endl;
     return true;
 }
 
+// TODO: Combine with decode() ?
 // Execute the decoded operation
 bool chip8::execute()
 {
